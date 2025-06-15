d44d2b14265807d9cd5486c07b7c5ec05af1d4f7
genesisgaytan07 <genesissgaytan@gmail.com>
2025-06-14 21:57:57 -0500

    updates
    adding some javascript

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,41 +1,48 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function LoginForm(){
+   
+    const [email, setEmail] = useState(" ");
+    const [password, setPassword] = useState(" ");
+        
+    
+ 
     return(
-
-        <div className='bg-white rounded-3xl px-10 py-10' >
+        <div className='px-10 py-10 bg-white rounded-3xl' >
             
             <p className='text-purple-950'>Enter your login information.</p>
-            <h1 className=' text-6xl font-semibold mb-8 text-purple-950'>Welcome</h1>
+            <h1 className='mb-8 text-6xl font-semibold text-purple-950'>Welcome</h1>
             <div>
                 
                 <input
-                    className='w-full  mb-2 px-2 py-1 rounded-xl border-2 border-gray-100 focus:outline-none focus:ring-2 focus:ring-yellow-400'
+                    className='w-full px-2 py-1 mb-2 border-2 border-gray-100 rounded-xl focus:outline-none focus:ring-2 focus:ring-yellow-400'
                     placeholder= 'Email address'
                     type='email'
+                    onChange={(e)=>setEmail(e.target.vaule)}
                 />
             </div>
             
             <div>
                 
                 <input
-                    className='w-full mb-2 px-2 py-1 rounded-xl border-2 border-gray-100 focus:outline-none focus:ring-2 focus:ring-yellow-400'
+                    className='w-full px-2 py-1 mb-2 border-2 border-gray-100 rounded-xl focus:outline-none focus:ring-2 focus:ring-yellow-400'
                     placeholder= 'Password'
                     type = 'password'
+                    onChange={(e)=>setPassword(e.target.vaule)}
                 />
             </div>
 
             
            
-            <div className="flex justify-center items-center">
+            <div className="flex items-center justify-center">
                 <button className='active:scale-[.98] text-blue-700 text-sm hover:underline'>Forgot Password?</button>
             </div>
             
-            <div className="flex justify-center mt-2 flex-col gap-y-4">
+            <div className="flex flex-col justify-center mt-2 gap-y-4">
                 <button className=" active:scale-[.98] text-white font-bold bg-yellow-400 hover:bg-yellow-500 rounded-full ">Login</button>
             </div>
 
-            <div className="flex justify-center mt-2 items-center text-sm space-x-2">
+            <div className="flex items-center justify-center mt-2 space-x-2 text-sm">
                 <p>Don't have an account?</p>
                 <button className="active:scale-[.98] text-blue-700 hover:underline">Sign up</button>
             </div>
